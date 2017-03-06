eb43cf8096ceee5d0fa3451eab8de502c9c745fc
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2017-03-07 00:56:22 +0530

    Added SQLData.hs

---
+++ src/JDBC/Types/SQLData.hs
@@ -0,0 +1,9 @@
+import JDBC.Types.SQLData
+
+import Java
+
+foreign import java unsafe "@interface getSQLTypeName" getSQLTypeNameSQLData :: Java SQLData JString
+
+foreign import java unsafe "@interface readSQL" readSQLSQLData :: SQLInput -> JString -> Java SQLData ()
+
+foreign import java unsafe "@interface writeSQL" writeSQLSQLData :: SQLOutput -> Java SQLData ()
