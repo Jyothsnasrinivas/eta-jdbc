c063de521baab3795f46a28233d5ae7fa159c294
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2017-03-14 21:07:11 +0530

    Finished Wrappers for Struct.hs

--- src/JDBC/Types/Struct.hs
+++ src/JDBC/Types/Struct.hs
@@ -2,9 +2,15 @@ import JDBC.Types.Struct
 
 import Java
 
-foreign import java unsafe "@interface getAttributes" getAttributesStruct :: Java Struct ObjectArray --TODO
+foreign import java unsafe "@interface getAttributes" getAttributesStruct_ :: Java Struct JObjectArray
 
-foreign import java unsafe "@interface getAttributes" getAttributesStruct2 :: Map JString (JClass b)
-                                                -> Java Struct ObjectArray --TODO
+getAttributesStruct :: Java Struct JObjectArray
+getAttributesStruct = fmap fromJava (getAttributesStruct_)
+
+foreign import java unsafe "@interface getAttributes" getAttributesStruct2_ ::
+ Map JString (JClass b) -> Java Struct ObjectArray
+
+getAttributesStruct2 :: [(JString, JClass b)] -> Java Struct JObjectArray
+getAttributesStruct2 t = fmap fromJava (toJava (getAttributesStruct2_ t))
 
 foreign import java unsafe "@interface getSQLTypeName" getSQLTypeNameStruct :: Java Struct JString
