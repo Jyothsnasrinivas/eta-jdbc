c13315b20006063074fb36f079c6b2f182c2e14a
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2017-03-14 19:37:58 +0530

    Finished Wrappers for Clob.hs

--- src/JDBC/Types/Clob.hs
+++ src/JDBC/Types/Clob.hs
@@ -8,10 +8,10 @@ foreign import java unsafe "@interface getAsciiStream" getAsciiStreamClob :: Jav
 
 foreign import java unsafe "@interface getCharacterStream" getCharacterStreamClob :: Java Clob Reader
 
-foreign import java unsafe "@interface getCharacterStream" getCharacterStreamClob2 :: Int64
-                                                          -> Int64 -> Java Clob Reader
+foreign import java unsafe "@interface getCharacterStream" getCharacterStreamClob2 ::
+  Int64 -> Int64 -> Java Clob Reader
 
-foreign import java unsafe "@interface getSubString" getSubStringClob :: Int64 -> Int -> Java Clob JString -- TODO
+foreign import java unsafe "@interface getSubString" getSubStringClob :: Int64 -> Int -> Java Clob JString
 
 foreign import java unsafe "@interface length" lengthClob :: Java Clob Int64
 
@@ -25,7 +25,14 @@ foreign import java unsafe "@interface setCharacterStream" setCharacterStreamClo
 
 foreign import java unsafe "@interface setString" setStringClob :: Int64 -> JString -> Java Clob Int
 
-foreign import java unsafe "@interface setString" setStringClob2 :: Int64 -> JByteArray
-                                                               -> Int -> Int -> Java Clob Int -- TODO
+foreign import java unsafe "@interface setString" setStringClob2_ ::
+  Int64 -> JByteArray -> Int -> Int -> Java Clob Int
+
+--Wrapper
+
+setStringClob2 :: Int64 -> [Byte] -> Int -> Int -> Java Clob Int
+setStringClob2 t1 t2 t3 t4 = setStringClob2_ t1 (toJava t2) t3 t4
+
+--End Wrapper
 
 foreign import java unsafe "@interface tuncate" truncateClob :: Int64 -> Java Clob ()
