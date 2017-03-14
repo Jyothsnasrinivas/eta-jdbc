19658db92b40ae0a610a2157dcab590edb191932
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2017-03-14 19:33:45 +0530

    Finished Wrappers for Array.hs

--- src/JDBC/Types/Array.hs
+++ src/JDBC/Types/Array.hs
@@ -1,7 +1,18 @@
-module JDBC.Types.Array where
+module JDBC.Types.Array
+  ( free,
+  getArray,
+  getArray2,
+  getArray3,
+  getArray4,
+  getBaseType,
+  getBaseTypeName,
+  getResultSet,
+  getResultSet2,
+  getResultSet3,
+  getResultSet4)
+where
 
 import Java
-
 
 foreign import java unsafe "@interface free" free :: Java Array ()
 
@@ -9,20 +20,48 @@ foreign import java unsafe "@interface getArray" getArray :: Java Array Object
 
 foreign import java unsafe "@interface getArray" getArray2 :: Int64 -> Int -> Java Array Object
 
-foreign import java unsafe "@interface getArray" getArray3 :: Int64 -> Int
-                                                           -> Map JString (JClass b) -> Java Array Object
+foreign import java unsafe "@interface getArray"
+  getArray3_ :: Int64 -> Int -> Map JString (JClass b) -> Java Array Object
 
-foreign import java unsafe "@interface getArray" getArray4 :: Map JString (JClass b) -> Java Array Object
+--Wrapper
+
+getArray3 :: Int64 -> Int -> [(JString, JClass b)] -> Java Array Object
+getArray3 t1 t2 t3 = getArray3_ t1 t2 (toJava t3)
+
+--End Wrapper
+
+foreign import java unsafe "@interface getArray" getArray4_ :: Map JString (JClass b) -> Java Array Object
+
+--Wrapper
+
+getArray4 :: [(JString, JClass b)] -> Java Array Object
+getArray4 t = getArray4_ (toJava t)
+
+--End Wrapper
 
 foreign import java unsafe "@interface getBaseType" getBaseType :: Java Array Int
 
-foreign import java unsafe "@interface getBaseTypeName" getBaseType :: Java Array JString
+foreign import java unsafe "@interface getBaseTypeName" getBaseTypeName :: Java Array JString
 
 foreign import java unsafe "@interface getResultSet" getResultSet :: Java Array ResultSet
 
 foreign import java unsafe "@interface getResultSet" getResultSet2 :: Int64 -> Int -> Java Array ResultSet
 
-foreign import java unsafe "@interface getResultSet" getResultSet3 :: Int64 -> Int
-                                                           -> Map JString (JClass b) -> Java Array ResultSet
+foreign import java unsafe "@interface getResultSet"
+getResultSet3_ :: Int64 -> Int -> Map JString (JClass b) -> Java Array ResultSet
 
-foreign import java unsafe "@interface getResultSet4" getResultSet4 :: Map JString (JClass b) -> Java Array ResultSet                                                           
+--Wrapper
+
+getResultSet3 :: Int64 -> Int -> [(JString, JClass b)] -> Java Array ResultSet
+getResultSet3 t1 t2 t3 = getResultSet3_ t1 t2 (toJava t3)
+
+--End Wrapper
+
+foreign import java unsafe "@interface getResultSet4" getResultSet4_ :: Map JString (JClass b) -> Java Array ResultSet
+
+--Wrapper
+
+getResultSet4 :: [(JString, JClass b)] -> Java Array ResultSet
+getResultSet4 t = getResultSet4_ (toJava t)
+
+--End Wrapper
--- src/JDBC/Types/Blob.hs
+++ src/JDBC/Types/Blob.hs
@@ -9,19 +9,47 @@ foreign import java unsafe "@interface getBinaryStream" getBinaryStreamBlob :: J
 foreign import java unsafe "@interface getBinaryStream" getBinaryStreamBlob2 :: Int64 -> Int64
                                                                              -> Java Blob InputStream
 
-foreign import java unsafe "@interface getBytes" getBytesBlob :: Int64 -> Int -> Java Blob JByteArray -- TODO
+foreign import java unsafe "@interface getBytes" getBytesBlob_ :: Int64 -> Int -> Java Blob JByteArray
+
+--Wrapper
+
+getBytesBlob :: Int64 -> Int -> Java Blob [Byte]
+getBytesBlob t1 t2 = fmap fromJava (getBytesBlob_ t1 t2)
+
+--End Wrapper
 
 foreign import java unsafe "@interface length" lengthBlob :: Java Blob Int64
 
 foreign import java unsafe "@interface position" positionBlob :: Blob -> Int64 -> Java Blob Int64
 
-foreign import java unsafe "@interface position" positionBlob2 :: JByteArray -> Int64 -> Java Blob Int64
+foreign import java unsafe "@interface position" positionBlob2_ :: JByteArray -> Int64 -> Java Blob Int64
+
+--Wrapper
+
+positionBlob2 :: [Byte] -> Int64 -> Java Blob Int64
+positionBlob2 t1 t2 = positionBlob2_ (toJava t1) t2
+
+--End Wrapper
 
 foreign import java unsafe "@interface setBinaryStream" setBinaryStreamBlob :: Int64 -> Java Blob OutputStream
 
-foreign import java unsafe "@interface setBytes" setBytesBlob :: Int64 -> JByteArray -> Java Blob Int -- TODO
+foreign import java unsafe "@interface setBytes" setBytesBlob_ :: Int64 -> JByteArray -> Java Blob Int
 
-foreign import java unsafe "@interface setBytes" setBytesBlob2 :: Int64 -> JByteArray
-                                                               -> Int -> Int -> Java Blob Int -- TODO
+--Wrapper
 
-foreign import java unsafe "@interface tuncate" truncateBlob :: Int64 -> Java Blob ()                                                               
+setBytesBlob :: Int64 -> [Byte] -> Java Blob Int
+setBytesBlob t1 t2 = setBytesBlob_ t1 (toJava t2)
+
+--End Wrapper
+
+foreign import java unsafe "@interface setBytes" setBytesBlob2_ ::
+  Int64 -> JByteArray -> Int -> Int -> Java Blob Int
+
+--Wrapper
+
+setBytesBlob2 :: Int64 -> [Byte] -> Int -> Int -> Java Blob Int
+setBytesBlob2 t1 t2 = setBytesBlob2_ t1 (toJava t2)
+
+
+
+foreign import java unsafe "@interface tuncate" truncateBlob :: Int64 -> Java Blob ()
